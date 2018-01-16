7d763ee1a1d48b6679c8346781d8b9d7a9d780b4
Calvin Vette <calvin.vette@triveratech.com>
2018-01-16 18:18:03 +0000

    Added Lab 4 with @Input customerId and CustomerComponent.customers static hashtable

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Customer} from './Customer';
 
 @Component({
@@ -7,14 +7,28 @@ import {Customer} from './Customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  private static customers: any = {
+    "1234": new Customer(1234, 'Harry', 'Potter', 'harry.potter@hogwarts.ac.uk', '+44 0206 591-3155'),
+    "1235": new Customer(1235, 'Ron', 'Weasley', 'ron.weasley@hogwarts.ac.uk', '+44 0206 591-3155'),
+    "1236": new Customer(1236, 'Hermione', 'Granger', 'hermione.granger@hogwarts.ac.uk', '+44 0206 591-3155')
+  };
+  // private static customers: Customer[] = [
+  //   new Customer(1234, 'Harry', 'Potter', 'harry.potter@hogwarts.ac.uk', '+44 0206 591-3155'),
+  //   new Customer(1235, 'Ron', 'Weasley', 'ron.weasley@hogwarts.ac.uk', '+44 0206 591-3155'),
+  //   new Customer(1236, 'Hermione', 'Granger', 'hermione.granger@hogwarts.ac.uk', '+44 0206 591-3155')
+  // ];
+
+  private _customerId: number = 1234;
   private _customer: Customer = null;
 
   constructor() {
-    this.customer = new Customer(1234, 'Harry', 'Potter', 'harry.potter@hogwarts.ac.uk', '+44 0206 591-3155');
-    this.customer.birthDate = new Date(1980, 6, 31);
+   
   }
 
   ngOnInit() {
+    this.customer = CustomerComponent.customers[this.customerId];
+    this.customer.birthDate = new Date(1979, 8, 17);
+    // this.customer.birthDate = new Date(1980, 6, 31);
   }
 
   get customer(): Customer {
@@ -23,5 +37,14 @@ export class CustomerComponent implements OnInit {
 
   set customer(value: Customer) {
     this._customer = value;
+  }
+
+  get customerId(): number {
+    return this._customerId;
+  }
+
+  @Input()
+  set customerId(value: number) {
+    this._customerId = value;
   }
 }
