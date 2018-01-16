c866896544cdb60c51029666baa34cd07f872d62
Calvin Vette <calvin.vette@triveratech.com>
2018-01-16 20:14:08 +0000

    Lab 4 with static Customer List in CustomerComponent and *ngFor in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Customer} from './customer/Customer';
+import {CustomerComponent} from './customer/customer.component';
 
 @Component({
   selector: 'weasley-root',
@@ -8,7 +10,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   private _title = 'Weasley\'s Wizarding Wheezes';
   private _now: Date = new Date();
-
+  private _customerList: Customer[] = CustomerComponent.customerList;
 
   get title(): string {
     return this._title;
@@ -24,5 +26,13 @@ export class AppComponent {
 
   set now(value: Date) {
     this._now = value;
+  }
+
+  get customerList(): Customer[] {
+    return this._customerList;
+  }
+
+  set customerList(value: Customer[]) {
+    this._customerList = value;
   }
 }
--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -8,21 +8,22 @@ import {Customer} from './Customer';
 })
 export class CustomerComponent implements OnInit {
   private static customers: any = {
-    "1234": new Customer(1234, 'Harry', 'Potter', 'harry.potter@hogwarts.ac.uk', '+44 0206 591-3155'),
-    "1235": new Customer(1235, 'Ron', 'Weasley', 'ron.weasley@hogwarts.ac.uk', '+44 0206 591-3155'),
-    "1236": new Customer(1236, 'Hermione', 'Granger', 'hermione.granger@hogwarts.ac.uk', '+44 0206 591-3155')
+    '1234': new Customer(1234, 'Harry', 'Potter', 'harry.potter@hogwarts.ac.uk', '+44 0206 591-3155'),
+    '1235': new Customer(1235, 'Ron', 'Weasley', 'ron.weasley@hogwarts.ac.uk', '+44 0206 591-3155'),
+    '1236': new Customer(1236, 'Hermione', 'Granger', 'hermione.granger@hogwarts.ac.uk', '+44 0206 591-3155')
   };
-  // private static customers: Customer[] = [
-  //   new Customer(1234, 'Harry', 'Potter', 'harry.potter@hogwarts.ac.uk', '+44 0206 591-3155'),
-  //   new Customer(1235, 'Ron', 'Weasley', 'ron.weasley@hogwarts.ac.uk', '+44 0206 591-3155'),
-  //   new Customer(1236, 'Hermione', 'Granger', 'hermione.granger@hogwarts.ac.uk', '+44 0206 591-3155')
-  // ];
 
-  private _customerId: number = 1234;
+  private static _customerList: Customer[] = [
+    new Customer(1234, 'Harry', 'Potter', 'harry.potter@hogwarts.ac.uk', '+44 0206 591-3155'),
+    new Customer(1235, 'Ron', 'Weasley', 'ron.weasley@hogwarts.ac.uk', '+44 0206 591-3155'),
+    new Customer(1236, 'Hermione', 'Granger', 'hermione.granger@hogwarts.ac.uk', '+44 0206 591-3155')
+  ];
+
+  private _customerId: number = -1;
   private _customer: Customer = null;
 
   constructor() {
-   
+
   }
 
   ngOnInit() {
@@ -46,5 +47,14 @@ export class CustomerComponent implements OnInit {
   @Input()
   set customerId(value: number) {
     this._customerId = value;
+  }
+
+
+  static get customerList(): Customer[] {
+    return CustomerComponent._customerList;
+  }
+
+  static set customerList(value: Customer[]) {
+    CustomerComponent._customerList = value;
   }
 }
